f52c12cd25b529ed695b3e692cc2492c5d938357
Mavi-Esquerdo <marciahelenaesquerdo@gmail.com>
2024-06-14 12:27:28 +0000

    save

---
+++ app/src/main/java/application/model/Genero.java
@@ -0,0 +1,42 @@
+package application.model;
+ 
+import java.util.Set;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+ 
+@Entity
+@Table(name = "generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String nome;
+ 
+    @OneToMany(mappedBy = "genero")
+    private Set<Jogos> jogos;
+ 
+    public Set<Jogos> getJogos() {
+        return jogos;
+    }
+    public void setLivros(Set<Jogos> jogos) {
+        this.jogos = jogos;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
